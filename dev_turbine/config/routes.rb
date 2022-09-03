052a4f2a75218a08701b3cfaad4784009b998559
killacan <kameron000@yahoo.com>
2022-09-02 17:00:32 -0700

    testing

--- dev_turbine/config/routes.rb
+++ dev_turbine/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   
   # post 'api/test', to: 'application#test'
+  # test.
   
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
